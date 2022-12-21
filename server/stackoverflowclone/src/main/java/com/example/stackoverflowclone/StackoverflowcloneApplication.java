26fa7af176e99cb08c14db3e06cff10a5f649a6e
eastmeet <ys932184@gmail.com>
2022-12-21 19:24:20 +0900

    [init] 프로젝트 생성: build gradle

---
+++ server/stackoverflowclone/src/main/java/com/example/stackoverflowclone/StackoverflowcloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.stackoverflowclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackoverflowcloneApplication.class, args);
+    }
+
+}
