bdd04051b0b47e10d86f7d04058d1c4fe3e834cb
Lee JaeHyeok <dhfif718@gmail.com>
2022-12-26 16:28:06 +0900

    [init] Test Code 기본 틀 작성, import 추가

--- server/stackoverflowclone/src/test/java/com/example/stackoverflowclone/domain/question/controller/QuestionControllerTest.java
+++ server/stackoverflowclone/src/test/java/com/example/stackoverflowclone/domain/question/controller/QuestionControllerTest.java
@@ -1,7 +1,67 @@
 package com.example.stackoverflowclone.domain.question.controller;
 
+import com.example.stackoverflowclone.domain.member.service.MemberService;
+import com.example.stackoverflowclone.domain.question.mapper.QuestionMapper;
+import com.example.stackoverflowclone.domain.question.service.QuestionService;
+import com.example.stackoverflowclone.domain.tag.service.TagService;
+import com.example.stackoverflowclone.domain.vote.service.QuestionVoteService;
+import com.example.stackoverflowclone.helper.StubData;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.*;
 
-class QuestionControllerTest {
+@WebMvcTest(QuestionController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
+public class QuestionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Gson gson;
+
+    @MockBean
+    private  MemberService memberService;
+
+    @MockBean
+    private  TagService tagService;
+
+    @MockBean
+    private  QuestionService questionService;
+
+    @MockBean
+    private  QuestionMapper questionMapper;
+
+    @MockBean
+    private  QuestionVoteService questionVoteService;
+
+    @Test
+    void createQuestion(){
+        StubData.MockQuestion.getRequestBody(HttpMethod.POST);
+
+    }
 
 }
---
+++ server/stackoverflowclone/src/test/java/com/example/stackoverflowclone/util/ApiDocumentUtils.java
@@ -0,0 +1,16 @@
+package com.example.stackoverflowclone.util;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+public interface ApiDocumentUtils {
+    static OperationRequestPreprocessor getRequestPreProcessor() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getResponsePreProcessor() {
+        return preprocessResponse(prettyPrint());
+    }
+}
