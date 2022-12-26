926ac975df4a9edbaf4a0c4b3c5dfc66ddb56a09
Lee JaeHyeok <dhfif718@gmail.com>
2022-12-26 15:51:45 +0900

    [init] Test package 정리

---
+++ server/stackoverflowclone/src/test/java/com/example/stackoverflowclone/domain/member/controller/MemberControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.stackoverflowclone.domain.member.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest {
+
+}
---
+++ server/stackoverflowclone/src/test/java/com/example/stackoverflowclone/domain/question/controller/QuestionControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.stackoverflowclone.domain.question.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionControllerTest {
+
+}
