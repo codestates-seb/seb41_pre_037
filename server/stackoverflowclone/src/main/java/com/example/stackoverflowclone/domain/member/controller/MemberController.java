7ce3c7cb5b125a37fe1360ce0fda3434cced4ad1
jongsic17 <qkrwhdtlr178@naver.com>
2022-12-27 19:38:47 +0900

    [feat] 날짜 수정중

--- server/stackoverflowclone/src/main/java/com/example/stackoverflowclone/domain/member/controller/MemberController.java
+++ server/stackoverflowclone/src/main/java/com/example/stackoverflowclone/domain/member/controller/MemberController.java
@@ -17,7 +17,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/users")
@@ -37,8 +41,52 @@ public class MemberController {
     }
     @GetMapping("/{member-id}/{username}") // TODO: username 수정필요
     public ResponseEntity getMemberProfile(@PathVariable("member-id") @Valid Long memberId){
-        System.out.println("들어옴");
         Member member = memberService.findByMember(memberId);
+            LocalDateTime now = LocalDateTime.now(); // 현재시간
+            LocalDateTime date = LocalDateTime.of(2022,12,25,0,0,0); // 생성시간
+        // date -> member 생성일, question 생성일, answer 생성일 클래스 하나로
+            System.out.println("현재시간 = "+now);
+            System.out.println("생성시간 = "+date);
+        long min = ChronoUnit.MINUTES.between(date, now);
+        long hour = ChronoUnit.HOURS.between(date, now);
+        long days = ChronoUnit.DAYS.between(date, now);
+        long month = ChronoUnit.MONTHS.between(date, now);
+        long years = ChronoUnit.YEARS.between(date, now);
+        // 프로필
+        System.out.println("================= 프로필 =================");
+        System.out.println("member for "+ days +" Day"); //
+        // member for 1개월 1일 x
+        // member for 1month 1day
+        System.out.println("Last seen this week");
+        System.out.println("Visited "+ days +" days," + days +" consecutive"); // 연속 방문 일수  -> ???
+
+
+
+        // if year 0이 아니면 year 쓰고
+        // elif y
+        // 질문 답변
+        System.out.println("================= 질문, 답변 =================");
+        System.out.println("질문, 답변 분 = "+ min%60 +" min ago"); // 이어 붙여야하는데
+        System.out.println("질문, 답변 시간 = "+ hour%24 +" hour ago");
+        System.out.println("질문, 답변 일 = "+ days%30 +" days ago");
+        System.out.println("질문, 답변 월 = "+ month%12 +" month ago");
+        System.out.println("질문, 답변 년도 = "+ years%100 +" years ago");
+
+        //member -> 생성시간
+//            // 프로필
+//            System.out.println("member for "+ ChronoUnit.DAYS.between(member.getCreatedAt(), now) +" Day"); // 31일 이면 -> 1개월 1일 로 변환됨
+//            // member for 1개월 1일 x
+//            // member for 1month 1day
+//            System.out.println("Last seen this week");
+//            System.out.println("Visited "+ ChronoUnit.DAYS.between(member.getCreatedAt(), now) +" days," + ChronoUnit.DAYS.between(member.getCreatedAt(), now) +" consecutive");
+//
+//            // 질문 답변
+//            System.out.println("질문, 답변  = "+ ChronoUnit.MINUTES.between(member.getCreatedAt(), now) +" min ago"); // minusMinutes();
+//            System.out.println("질문, 답변  = "+ ChronoUnit.HOURS.between(member.getCreatedAt(), now) +" hour ago"); // 달이 한글로 나온다?
+//            System.out.println("질문, 답변  = "+ ChronoUnit.DAYS.between(member.getCreatedAt(), now) +" days ago");
+//            System.out.println("질문, 답변  = "+ ChronoUnit.MONTHS.between(member.getCreatedAt(), now) +" month ago");
+
+
         return new ResponseEntity<>(
                 new DataResponseDto<>(mapper.memberTomemberProfileResponse(member)),
                 HttpStatus.OK );
