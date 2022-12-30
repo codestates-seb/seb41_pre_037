90a5f85b35ca9c22841bff41ec5faddf69f43042
Yeon <yeonhwan619@gmail.com>
2022-12-30 17:59:17 +0900

    [refactor] signUp, Main, RightSidebar 리팩토링 작업중 + google Login시에도 userInfo 받아놓기 위해 기본 코드 추가

--- client/src/Pages/Token.jsx
+++ client/src/Pages/Token.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useIsLoginStore } from "../store/loginstore";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Token() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const { isLogin, setIsLogin } = useIsLoginStore((state) => state);
   const navigate = useNavigate();
 
@@ -11,12 +13,22 @@ export default function Token() {
     const accessToken = searchParams.get('access_token');
     if(accessToken) {
       sessionStorage.setItem('accesstoken', accessToken);
+      // user/login 로 
       setIsLogin(true);
       navigate('/');
     }
   }, []);
 
+  const fetchUserInfo = () => {
+    const accessToken = sessionStorage.getItem('accesstoken');
+    const headers = {
+      'AUTHORIZATION' : `Bearer ${accessToken}`
+    }
+    return axios.get(`${process.env.REACT_APP_SERVER_URI}users/token`, {headers});
+  }
 
+
+  useQuery({queryKey:['getUserInfo'], queryFn:fetchUserInfo, enabled: isLogin, onSuccess:(data) => {console.log(data)}})
 
   return (
     <>
--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ root.render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
