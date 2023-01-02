c2050f0869360fa19b887494a507a628252dee34
Yeon <yeonhwan619@gmail.com>
2023-01-02 14:02:44 +0900

    [fix] dateCalc 함수 사용성 확대

--- client/src/utils/dateCalc.js
+++ client/src/utils/dateCalc.js
@@ -1,16 +1,22 @@
 const dateCalc = (date) => {
   const now = new Date(Date.now());
   const createdAt = new Date(date);
-  const timeDiff = new Date(now - createdAt);
-  if (timeDiff.getDate() >= 365) {
-    return `asked ${Math.floor(timeDiff.getDate() / 365)} years ago`;
-  } 
-  else if (timeDiff.getDate() > 1) {
-    return `asked ${timeDiff.getDate()} days ago`;
+  const hoursDiff = Math.abs(now.getHours() - createdAt.getHours());
+  const daysDiff = new Date(now - createdAt).getDate();
+  const yearsDiff = Math.abs(now.getFullYear() - createdAt.getFullYear());
+
+  if(yearsDiff > 1) {
+    return  yearsDiff === 1 ? `${yearsDiff} year ago` : `${yearsDiff} years ago`;
   } 
   else {
-    return `asked ${timeDiff.getHours()} hours ago`;
+    if(daysDiff > 1) {
+      return daysDiff === 1 ? `${daysDiff} day ago` : `${daysDiff} days ago`;
+    } 
+    else {
+      return  hoursDiff === 1 ? `${hoursDiff} hour ago` : `${hoursDiff} hours ago`;
+    }
   }
+
 };
 
 export default dateCalc;
